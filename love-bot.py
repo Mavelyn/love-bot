10cd84eb715d1096f4502b533453a7cf706aaff9
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2021-02-19 00:15:53 -0500

    Replaced client with bot subclass

--- love-bot.py
+++ love-bot.py
@@ -1,15 +1,28 @@
 import os
-
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-@client.event 
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+bot = commands.Bot(command_prefix='!')
 
-client.run(TOKEN)
+movies = []
+date_ideas = []
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    
+
+@bot.command(name="movies")
+async def movie_list(ctx, movie):
+    movies.append(movie)
+    await ctx.send(movies)
+
+
+bot.run(TOKEN)

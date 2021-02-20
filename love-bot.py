7bcba0dbd0e4ce26d61f5550b636258129ca90c3
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2021-02-20 01:34:58 -0500

    Modified to work on AWS.

--- love-bot.py
+++ love-bot.py
@@ -1,10 +1,14 @@
 import os
 import discord
+import redis
 from dotenv import load_dotenv
 from discord.ext import commands
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+
+redis_server = redis.Redis()
+TOKEN = str(redis_server.get('DISCORD_TOKEN').decode('utf-8'))
 
 client = discord.Client()
 
@@ -12,17 +16,33 @@ bot = commands.Bot(command_prefix='!')
 
 movies = []
 date_ideas = []
+restaurants = []
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-    
+
 
 @bot.command(name="movies")
 async def movie_list(ctx, movie):
     movies.append(movie)
     await ctx.send(movies)
+
+
+@bot.command(name="dateideas")
+async def date_ideas(ctx):
+    await ctx.send(date_ideas)
+
+
+@bot.command(name="restaurants")
+async def restaurant_list(ctx):
+    await ctx.send(restaurants)
+
+
+@bot.command(name="cutestboi")
+async def cutie(ctx):
+    await ctx.send("Richard Wei uwu")
 
 
 bot.run(TOKEN)

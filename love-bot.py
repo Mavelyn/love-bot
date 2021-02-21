33798e23663466a39e9dd77311f26aa798ae4ac2
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2021-02-21 00:45:00 -0500

    Formatted bot responses with discord embed.

--- love-bot.py
+++ love-bot.py
@@ -22,12 +22,21 @@ async def add_movie(ctx, *, movie):
 
 
 @bot.command(name="movies")
-async def movie_list(ctx):
+async def list_movies(ctx):
     if r.exists("Movies"):
         movies = r.lrange("Movies", 0, -1)
-        await ctx.send(movies)
+        embed = discord.Embed(
+            title="Our Movie List",
+            color=discord.Color.blue()
+        )
+        for movie in movies:
+            embed.add_field(name=movie, inline=False)
+        await ctx.send(embed=embed)
     else:
         await ctx.send("No movies found.")
+
+
+
 
 
 @bot.command(name="adddateidea")
@@ -37,10 +46,16 @@ async def add_date_idea(ctx, *, date_idea):
 
 
 @bot.command(name="dateideas")
-async def date_ideas(ctx):
+async def list_date_ideas(ctx):
     if r.exists("Date Ideas"):
         date_ideas = r.lrange("Date Ideas", 0, -1)
-        await ctx.send(date_ideas)
+        embed = discord.Embed(
+            title="Date Ideas",
+            color=discord.Color.blue()
+        )
+        for idea in date_ideas:
+            embed.add_field(name=idea, inline=False)
+        await ctx.send(embed=embed)
     else:
         await ctx.send("No date ideas found.")
 
